cf87aa527d5df882bb424d3bcf0f99652cd64ad4
Sanja Kovacic <santos.kovacic@gmail.com>
2019-04-01 16:00:40 +0200

    promjene

---
+++ 4.2.1.Narudzba_artikala/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _4._2._1.Narudzba_artikala
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Modeli/Controllers/TocanOdgovorController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Modeli.Controllers
+{
+    public class TocanOdgovorController : Controller
+    {
+      public ViewResult ProvjeriOdgovor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult ProvjeriOdgovor(string odgovor)
+        {
+            if (!string.IsNullOrEmpty(odgovor))
+            {
+                if (odgovor == "Bruxelles")
+                {
+                    rezultat = "Točan odgovor!";
+                    return View((object)rezultat);
+                }
+                else
+                {
+                    rezultat = "Netočan odgovor!";
+                    return View((object)rezultat);
+                }
+            }
+            else
+            {
+                rezultat = "Odgovor ne postoji!";
+                return View((object)rezultat);
+            }
+        }
+    }
+}
