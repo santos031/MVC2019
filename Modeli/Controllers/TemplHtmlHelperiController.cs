5000e6a1e2e31ec3bb77c630f2d15be9b7685c8a
Sanja Kovacic <santos.kovacic@gmail.com>
2019-04-04 17:48:16 +0200

    templated helper metode

---
+++ Modeli/Controllers/TemplHtmlHelperiController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Modeli.Models;
+
+namespace Modeli.Controllers
+{
+    public class TemplHtmlHelperiController : Controller
+    {
+        // GET: TemplHtmlHelperi
+        public ViewResult HtmlEditorView()
+        {
+            return View(new OsobaTempl());
+        }
+
+        [HttpPost]
+        public ViewResult HtmlEditorView(OsobaTempl osoba)
+        {
+            return View("HtmlLabelDisplay", osoba);
+        }
+
+        public ViewResult EditorModelView()
+        {
+            return View(new OsobaTempl());
+        }
+
+        [HttpPost]
+        public ViewResult EditorModelView(OsobaTempl osoba)
+        {
+            return View("HtmlLabelDisplay", osoba);
+        }
+    }
+}
