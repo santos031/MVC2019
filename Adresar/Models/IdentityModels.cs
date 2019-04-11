b3945b5278f980de0cdd5679a578c9c5273d747f
Sanja Kovacic <santos.kovacic@gmail.com>
2019-04-11 18:25:48 +0200

    LINQ to SQL

---
+++ Adresar/Models/IdentityModels.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Adresar.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    //dbContext je ORM
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false) 
+            //ruta za DefaultConnection se nalazi unutar Web.configa <connectionstring> 
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public virtual DbSet<Kontakt> Kontakti { get; set; }
+        //registriramo model u entity framework tako da klasu prijavljujemo kao svojstvo klase 
+        //koja naslijeđuje DbContext u obliku DbSet liste - u biti to je tablica i omogućili smo čitanje i pisanje
+    }
+}
