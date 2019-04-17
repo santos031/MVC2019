cc861f17fa53c573e374b025e966d5ebbfde857c
Sanja Kovacic <santos.kovacic@gmail.com>
2019-04-17 17:47:37 +0200

    wfc

---
+++ Adresar/Controllers/KontaktiWSController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Adresar.KontaktService;
+
+namespace Adresar.Controllers
+{
+    public class KontaktiWSController : Controller
+    {
+        // GET: KontaktiWS
+        public ActionResult Index()
+        {
+            KontaktServiceClient klijent = new KontaktServiceClient();
+
+            List<Kontakt> kontakti = klijent.DohvatiAktivneKontakte().ToList();
+    
+            return View(kontakti);
+        }
+    }
+}
